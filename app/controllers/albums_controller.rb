f5cb23b36ce62891d8904b421e8c8ab222812517
toxox <toxa1993@gmail.com>
2013-05-31 21:54:27 +0300

    Fix bug in Album#index.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,13 +4,12 @@ class AlbumsController < ApplicationController
   # GET /albums
   # GET /albums.json
   def index
-    get_views_variables
+    @albums = Album.all
   end
 
   # GET /albums/1
   # GET /albums/1.json
   def show
-    get_views_variables
   end
 
   # GET /albums/new
@@ -72,14 +71,5 @@ class AlbumsController < ApplicationController
     def album_params
       params.require(:album).permit(:artist_id, :album_title, :genre_id,
         :producer_id, :date_released, :label_id, :cover)
-    end
-
-    def get_views_variables
-    @albums = Album.all
-    @albums.each do |album|
-      @album_genre = Genre.find(album.genre_id).name
-      @album_producer = Producer.find(album.producer_id).name
-      @album_label = Label.find(album.label_id).name
-    end
     end
 end
